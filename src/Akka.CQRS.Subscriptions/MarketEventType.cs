79765d365fa4fbf65e1111cbdb57778d23fb96c9
Aaron Stannard <aaron@petabridge.com>
2019-05-22 12:24:23 -0500

    implementing market event subscriptions

---
+++ src/Akka.CQRS.Subscriptions/MarketEventType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Akka.CQRS.Subscriptions
+{
+    /// <summary>
+    /// The type of market event we're interested in.
+    /// </summary>
+    public enum MarketEventType
+    {
+        VolumeChange,
+        PriceChange,
+    }
+}
