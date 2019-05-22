55a8bcf9fff8bcdb181084201bf8c9b51f2a6a32
Aaron Stannard <aaron@petabridge.com>
2019-05-22 18:04:31 -0500

    integrating MarketSubscribe events into custom Petabridge.Cmd palette (#5)
    * integrating MarketSubscribe events into custom Petabridge.Cmd palette
    
    * fixed bugs with Petabridge.Cmd responses for price

--- src/Akka.CQRS.Pricing.Cli/PriceCmdRouter.cs
+++ src/Akka.CQRS.Pricing.Cli/PriceCmdRouter.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using Akka.Actor;
 using Akka.CQRS.Pricing.Commands;
 using Akka.CQRS.Pricing.Views;
+using Akka.Event;
 using Petabridge.Cmd;
 using Petabridge.Cmd.Host;
 
@@ -20,6 +21,7 @@ namespace Akka.CQRS.Pricing.Cli
     /// </summary>
     public sealed class PriceCmdRouter : CommandHandlerActor
     {
+        private readonly ILoggingAdapter _log = Context.GetLogger();
         private IActorRef _priceViewMaster;
 
         public PriceCmdRouter(IActorRef priceViewMaster) : base(PricingCmd.PricingCommandPalette)
@@ -38,21 +40,32 @@ namespace Akka.CQRS.Pricing.Cli
             Process(PricingCmd.PriceHistory.Name, (command, arguments) =>
             {
                 var tickerSymbol = arguments.ArgumentValues("symbol").Single();
-                var getPriceTask = _priceViewMaster.Ask<PriceHistory>(new GetPriceHistory(tickerSymbol), TimeSpan.FromSeconds(5));
+                var getPriceTask = _priceViewMaster.Ask<PriceAndVolumeSnapshot>(new FetchPriceAndVolume(tickerSymbol), TimeSpan.FromSeconds(5));
                 var sender = Sender;
 
                 // pipe happy results back to the sender only on successful Ask
                 getPriceTask.ContinueWith(tr =>
                 {
-                    return Enumerable.Select(tr.Result.HistoricalPrices, x => new CommandResponse(x.ToString(), false))
-                        .Concat(new []{ CommandResponse.Empty });
-                }, TaskContinuationOptions.OnlyOnRanToCompletion).PipeTo(sender);
+                    try
+                    {
+                        if (tr.Result.PriceUpdates.Length == 0)
+                            return new[]
+                                {new CommandResponse($"No historical price data available for [{tr.Result.StockId}]")};
 
-                // pipe unhappy results back to sender on failure
-                getPriceTask.ContinueWith(tr => 
-                        new ErroredCommandResponse($"Error while fetching price history for {tickerSymbol} - " +
-                                                   $"timed out after 5s"), TaskContinuationOptions.NotOnRanToCompletion)
-                    .PipeTo(sender); ;
+                        return Enumerable.Select(tr.Result.PriceUpdates, x => new CommandResponse(x.ToString(), false))
+                            .Concat(new[] {CommandResponse.Empty});
+                    }
+                    catch (Exception ex)
+                    {
+                        _log.Error(ex, "Exception while returning price history for {0}", tickerSymbol);
+                        return new[] {CommandResponse.Empty};
+                    }
+
+                }).ContinueWith(tr =>
+                {
+                    foreach(var r in tr.Result)
+                        sender.Tell(r, ActorRefs.NoSender);
+                });
             });
         }
     }
