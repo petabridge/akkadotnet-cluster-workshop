c96e1f24d4ce8c20d2c5d5b036350787a7e290ce
Aaron Stannard <aaron@petabridge.com>
2019-05-22 12:12:23 -0500

    re-organizing subscriptions

---
+++ src/Akka.CQRS.Subscriptions/IMarketEventPublisher.cs
@@ -0,0 +1,12 @@
+﻿using Akka.CQRS.Pricing;
+
+namespace Akka.CQRS.Subscriptions
+{
+    /// <summary>
+    /// Abstraction for publishing data about <see cref="IMarketEvent"/> instances.
+    /// </summary>
+    public interface IMarketEventPublisher
+    {
+        void Publish(string tickerSymbol, ITradeEvent @event);
+    }
+}
