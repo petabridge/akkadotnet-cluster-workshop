816399bf26d30dbb86c86bfd57b2130ff0c1c1cc
Aaron Stannard <aaron@petabridge.com>
2019-05-23 15:07:06 -0500

    centralizing access to DevOps material

---
+++ src/Akka.CQRS.Infrastructure/AppBootstrap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Configuration;
+
+namespace Akka.CQRS.Infrastructure
+{
+    /// <summary>
+    /// Used to boostrap the start of <see cref="ActorSystem"/>s by injecting their configs
+    /// with the relevant bits and pieces.
+    /// </summary>
+    public static class AppBootstrap
+    {
+        public static Config BoostrapApplication(this Config c, AppBootstrapConfig appConfig)
+        {
+            return c;
+        }
+    }
+
+    public sealed class AppBootstrapConfig
+    {
+        public AppBootstrapConfig(bool needPersistence = true, bool needClustering = true)
+        {
+            NeedPersistence = needPersistence;
+            NeedClustering = needClustering;
+        }
+
+        public bool NeedPersistence { get; }
+
+        public bool NeedClustering { get; }
+    }
+}
