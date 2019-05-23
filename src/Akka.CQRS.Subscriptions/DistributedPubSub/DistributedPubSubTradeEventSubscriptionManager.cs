1cd4d42a0d153cee8ca5f5cb8754456fab636ffe
Aaron Stannard <aaron@petabridge.com>
2019-05-23 10:50:59 -0500

    implemented ActorTradeSubscriptionManager

--- src/Akka.CQRS.Subscriptions/DistributedPubSub/DistributedPubSubTradeEventSubscriptionManager.cs
+++ src/Akka.CQRS.Subscriptions/DistributedPubSub/DistributedPubSubTradeEventSubscriptionManager.cs
@@ -7,42 +7,6 @@ using Akka.Cluster.Tools.PublishSubscribe;
 namespace Akka.CQRS.Subscriptions.DistributedPubSub
 {
     /// <summary>
-    /// Abstract base class for working with <see cref="ITradeEventSubscriptionManager"/>.
-    /// </summary>
-    public abstract class TradeEventSubscriptionManagerBase : ITradeEventSubscriptionManager
-    {
-        public Task<TradeSubscribeAck> Subscribe(string tickerSymbol, IActorRef subscriber)
-        {
-            return Subscribe(tickerSymbol, TradeEventHelpers.AllTradeEventTypes, subscriber);
-        }
-
-        public Task<TradeSubscribeAck> Subscribe(string tickerSymbol, TradeEventType @event, IActorRef subscriber)
-        {
-            return Subscribe(tickerSymbol, new[] { @event }, subscriber);
-        }
-
-        public abstract Task<TradeSubscribeAck> Subscribe(string tickerSymbol, TradeEventType[] events, IActorRef subscriber);
-
-        public abstract Task<TradeUnsubscribeAck> Unsubscribe(string tickerSymbol, TradeEventType[] events,
-            IActorRef subscriber);
-
-        public Task<TradeUnsubscribeAck> Unsubscribe(string tickerSymbol, TradeEventType @event, IActorRef subscriber)
-        {
-            return Unsubscribe(tickerSymbol, new[] { @event }, subscriber);
-        }
-
-        public Task<TradeUnsubscribeAck> Unsubscribe(string tickerSymbol, IActorRef subscriber)
-        {
-            return Unsubscribe(tickerSymbol, TradeEventHelpers.AllTradeEventTypes, subscriber);
-        }
-
-        internal static string[] ToTopics(string tickerSymbol, TradeEventType[] events)
-        {
-            return events.Select(x => DistributedPubSubTradeEventTopicFormatter.ToTopic(tickerSymbol, x)).ToArray();
-        }
-    }
-
-    /// <summary>
     /// <see cref="ITradeEventSubscriptionManager"/> that uses the <see cref="DistributedPubSub.Mediator"/> under the hood.
     /// </summary>
     public sealed class DistributedPubSubTradeEventSubscriptionManager : TradeEventSubscriptionManagerBase
