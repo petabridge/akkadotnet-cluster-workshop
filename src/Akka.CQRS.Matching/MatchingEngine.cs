42ea1eb3fe2dd447b1062ba28f9f73b70a08c0b3
Aaron Stannard <aaron@petabridge.com>
2019-05-26 11:11:18 -0500

    fixed NRE with MatchingEngine

--- src/Akka.CQRS.Matching/MatchingEngine.cs
+++ src/Akka.CQRS.Matching/MatchingEngine.cs
@@ -61,8 +61,8 @@ namespace Akka.CQRS.Matching
         /// <returns>A new matching engine.</returns>
         public static MatchingEngine FromSnapshot(OrderbookSnapshot snapshot, ILoggingAdapter log = null, ITimestamper timestamper = null)
         {
-            return new MatchingEngine(snapshot.StockId, snapshot.Bids.ToDictionary(x => x.OrderId, y=> y), 
-                snapshot.Asks.ToDictionary(x => x.OrderId, y => y), log, timestamper);
+            return new MatchingEngine(snapshot.StockId, snapshot.Bids?.ToDictionary(x => x.OrderId, y=> y) ?? new Dictionary<string, Order>(), 
+                snapshot.Asks?.ToDictionary(x => x.OrderId, y => y) ?? new Dictionary<string, Order>(), log, timestamper);
         }
 
         /// <summary>
