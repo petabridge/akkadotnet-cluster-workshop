b5b863321a6863f1bddbc3dee54d16dc80704963
Aaron Stannard <aaron@petabridge.com>
2019-05-28 14:38:40 -0500

    lesson 2 - removed MongoDb connection string injection

--- src/Akka.CQRS.Infrastructure/AppBootstrap.cs
+++ src/Akka.CQRS.Infrastructure/AppBootstrap.cs
@@ -24,30 +24,15 @@ namespace Akka.CQRS.Infrastructure
         public static Config BoostrapApplication(this Config c, AppBootstrapConfig appConfig)
         {
             var config = c;
-            if (appConfig.NeedPersistence)
-            {
-                var mongoConnectionString = Environment.GetEnvironmentVariable("MONGO_CONNECTION_STR")?.Trim();
-                if (string.IsNullOrEmpty(mongoConnectionString))
-                {
-                    Console.WriteLine("ERROR! MongoDb connection string not provided. Can't start.");
-                    throw new ConfigurationException("ERROR! MongoDb connection string not provided. Can't start.");
-                }
-                else
-                {
-                    Console.WriteLine("Connecting to MongoDb at {0}", mongoConnectionString);
-                }
-
-                config = c.WithFallback(GetMongoHocon(mongoConnectionString));
-            }
 
             config = config
-                .WithFallback(GetOpsConfig())
+                .WithFallback(GetOpsConfig());
                 //.WithFallback(TradeEventSerializer.Config)
                 //.WithFallback(ClusterSharding.DefaultConfig())
                 //.WithFallback(DistributedData.DistributedData.DefaultConfig()) // needed for DData sharding
                 //.WithFallback(ClusterClientReceptionist.DefaultConfig())
                 //.WithFallback(DistributedPubSub.DefaultConfig())
-                .BootstrapFromDocker();
+                //.BootstrapFromDocker();
 
 
 #if PHOBOS
